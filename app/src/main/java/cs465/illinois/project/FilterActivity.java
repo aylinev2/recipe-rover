e1e93233f3f6f3f143c8a22893ae6da43936e74d
aylinevillegas <aylinev2@illinois.edu>
2019-12-06 13:12:08 -0600

    added filter buttons

--- app/src/main/java/cs465/illinois/project/FilterActivity.java
+++ app/src/main/java/cs465/illinois/project/FilterActivity.java
@@ -8,8 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.HashMap;
+
 public class FilterActivity extends AppCompatActivity implements View.OnClickListener {
     private Button applyButton;
+    private Button italian, vegetarian, dinner, breakfast;
+    private HashMap<String, Boolean> clicked = new HashMap<String, Boolean>() {{
+        put("italian", false); put("vegetarian", false); put("dinner", false); put("breakfast", false);
+    }};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,65 @@ public class FilterActivity extends AppCompatActivity implements View.OnClickLis
         applyButton.setVisibility(View.VISIBLE);
         applyButton.setBackgroundColor(Color.TRANSPARENT);
         applyButton.setOnClickListener(this);
+
+        italian = (Button) findViewById(R.id.italian);
+        italian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean click = clicked.get("italian");
+                if (click) {
+                    //Drawable image = (Drawable)getResources().getDrawable(R.drawable.rounded_button_three);
+                    italian.setBackgroundResource(R.drawable.rounded_button_four);
+                } else {
+                    //Drawable image = (Drawable)getResources().getDrawable(R.drawable.rounded_button_three);
+                    italian.setBackgroundResource(R.drawable.rounded_button_three);
+                }
+                clicked.put("italian", !click);
+            }
+        });
+
+        vegetarian = (Button) findViewById(R.id.vegetarian);
+        vegetarian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean click = clicked.get("vegetarian");
+                if (click) {
+                    vegetarian.setBackgroundResource(R.drawable.rounded_button_four);
+                } else {
+                    vegetarian.setBackgroundResource(R.drawable.rounded_button_three);
+                }
+                clicked.put("vegetarian", !click);
+            }
+        });
+
+        dinner = (Button) findViewById(R.id.dinner);
+        dinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean click = clicked.get("dinner");
+                if (click) {
+                    dinner.setBackgroundResource(R.drawable.rounded_button_four);
+                } else {
+                    dinner.setBackgroundResource(R.drawable.rounded_button_three);
+                }
+                clicked.put("dinner", !click);
+            }
+        });
+
+        breakfast = (Button) findViewById(R.id.breakfast);
+        breakfast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean click = clicked.get("breakfast");
+                if (click) {
+                    breakfast.setBackgroundResource(R.drawable.rounded_button_four);
+                } else {
+                    breakfast.setBackgroundResource(R.drawable.rounded_button_three);
+                }
+                clicked.put("breakfast", !click);
+            }
+        });
+
     }
 
     public void onClick(View v) {
